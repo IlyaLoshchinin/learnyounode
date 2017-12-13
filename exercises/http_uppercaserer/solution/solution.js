9f0562f6c66bcaf435b33eeab8571a450e74ed92
Illia Loshchinin <illia.loshchinin@gmail.com>
2017-12-13 23:43:34 +0100

    update exercises to use const and let instead of var #559

--- exercises/http_uppercaserer/solution/solution.js
+++ exercises/http_uppercaserer/solution/solution.js
@@ -1,14 +1,16 @@
-const http = require('http')
-const map = require('through2-map')
+'use strict'
 
-const server = http.createServer(function (req, res) {
+const http = require('http');
+const map = require('through2-map');
+
+const server = http.createServer(function(req, res) {
   if (req.method !== 'POST') {
-    return res.end('send me a POST\n')
+    return res.end('send me a POST\n');
   }
 
-  req.pipe(map(function (chunk) {
-    return chunk.toString().toUpperCase()
-  })).pipe(res)
-})
+  req.pipe(map(function(chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
+});
 
-server.listen(Number(process.argv[2]))
+server.listen(Number(process.argv[2]));
